8a7d066ae9265242ba3e7202b50ac07dda67d119
Loic Doubinine <ztec@riper.fr>
2013-03-31 13:35:19 -0700

    fix nameSpace and className

--- lib/mtChart/mtChart.php
+++ lib/mtChart/mtChart.php
@@ -1,17 +1,21 @@
 <?php
-namespace mtChart ;
+namespace MtChart ;
 
-class mtChart {
+class MtChartLoader {
 
     static  $loaded = false ;
-    static  $modules = array();
 
+    /**
+     * @param int $XSize
+     * @param int $YSize
+     * @return \mtChart
+     */
     static function getMtChart($XSize = 700, $YSize = 230){
         if(self::$loaded !== true){
             include_once __DIR__.'/src/mtChart.php';
             self::$loaded = true ;
         }
-        return new mtChart($XSize, $YSize);
+        return new \mtChart($XSize, $YSize);
     }
 }
 
